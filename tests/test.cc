062936fe3bab149bc2fc664301d8151868a3c874
Fabian Meumertzheim <fabian@meumertzhe.im>
2022-03-16 09:59:43 +0100

    Add support for the args argument

--- tests/test.cc
+++ tests/test.cc
@@ -3,6 +3,15 @@
 #include <string>
 
 int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cout << "argc: " << argc << std::endl;
+    return 1;
+  }
+  std::string arg = argv[1];
+  std::cout << "args: " << argv[1];
+  if (arg != "some_arg") {
+    return 1;
+  }
   const char* foo = std::getenv("FOO");
   if (foo == nullptr) {
     return 1;
